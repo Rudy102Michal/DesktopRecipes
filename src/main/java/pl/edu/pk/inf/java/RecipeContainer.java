b0ea50253b885ba3f6688f57bbdce18ef9dbae45
Bartek <barcie4@gmail.com>
2016-06-22 19:05:11 +0200

    ver 0.1 Bart

---
+++ src/main/java/pl/edu/pk/inf/java/RecipeContainer.java
@@ -0,0 +1,59 @@
+package pl.edu.pk.inf.java;
+
+/**
+ * Created by Operator on 2016-06-15.
+ */
+
+/**
+  ta klasa jest klasą kontenera. Będą w niej przechowywane wszystkie przepisy w trakcie trwania programu. Jest tutaj lista przepisów, iterator
+ oraz metoda do dodawania przepisów do tej listy. Poglądowa pętla wykorzystywania iteratora jest teraz a minie.
+ Po tej liście będziemy przechodzić szukając przepisów i robiąc wszystkie inne rzeczy. Udało mi sie wcisnąć dwa wzorce projektowe.
+
+
+
+ **/
+public class RecipeContainer implements Container{
+    static int count = 0;
+    private recipe[] RecipeList;
+    private static RecipeContainer instance = new RecipeContainer();
+
+    private RecipeContainer(){
+        RecipeList = new recipe[512];
+    }
+
+    public static RecipeContainer GetInstance(){
+        return instance;
+    }
+    public Iterator getIterator(){
+        return new RecipeIterator();
+    }
+
+    public void addRecipe(recipe a){
+        RecipeList[count++] = a;
+    }
+
+    private class RecipeIterator implements Iterator{
+
+        int index = 0;
+
+        public boolean hasNext() {
+            if(index < count){
+                return true;
+            }
+            return false;
+        }
+
+
+        public recipe next() {
+
+            if(this.hasNext()){
+                return RecipeList[index++];
+            }
+            return null;
+        }
+
+        public recipe current(){
+            return RecipeList[index];
+        }
+    }
+}
