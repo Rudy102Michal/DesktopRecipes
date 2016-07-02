a267c31e98a99c261657e5b70056e20c6a17eb5a
Michal Gornicki <mich.gor@wp.pl>
2016-07-02 20:33:44 +0200

    Switching between two scenes is now possible. Some binding done.

---
+++ src/main/java/Controllers/BrowseRecipesController.java
@@ -0,0 +1,18 @@
+package Controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Michał on 2016-07-02.
+ */
+public class BrowseRecipesController extends MainController {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        super.initialize(location, resources);
+
+    }
+}
